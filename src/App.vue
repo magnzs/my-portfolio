6eeb190369e22ba40eeac2fa389bdab8f05a5143
Magnno <93272214+magnzs@users.noreply.github.com>
2026-04-13 11:12:14 -0300

    feat: reset default CSS styles
    Use modern-normalize lib to reset the default styles.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const teste = ref(0)
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <button @click="teste++">{{ teste }}</button>
 </template>
 
-<style scoped></style>
+<style scoped>
+button {
+  width: 64px;
+  height: 32px;
+  background-color: #007bff;
+  border-radius: 8px;
+}
+</style>
